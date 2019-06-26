afa92e177375430bbac273425078edffa182c276
andrewjones <ajones@digitalbazaar.com>
2019-06-26 17:02:22 -0400

    Update peer deps and remove configs.

--- configs/permissions.js
+++
@@ -1,7 +0,0 @@
-/*!
- * Copyright (c) 2019 Digital Bazaar, Inc. All rights reserved.
- */
-const {config} = require('bedrock');
-
-/* eslint-disable-next-line no-unused-vars */
-const {permissions} = config.permission;
