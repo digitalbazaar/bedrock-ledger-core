e5ba1a9fd205f690a12bc48eaacabaed7b272c4b
Andrew L Jones <ajones@digitalbazaar.com>
2021-05-24 10:28:42 -0400

    Remove workerpool from config.
    Remove continuity settings.

--- configs/continuity.js
+++
@@ -1,9 +0,0 @@
-/*!
- * Copyright (c) 2019 Digital Bazaar, Inc. All rights reserved.
- */
-'use strict';
-
-const {config} = require('bedrock');
-
-config['ledger-consensus-continuity'].consensus.workerpool.enabled = true;
-config['ledger-consensus-continuity'].writer.debounce = 5000;
